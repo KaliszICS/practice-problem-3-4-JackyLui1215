02aef4a165af62634b9a4954628b5ea530b24a8c
JackyLui1215 <343344461@tdsb.ca>
2026-03-27 17:21:37 +0000

    finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,53 @@
+/**
+	* Lesson: Nested Ifs
+	* Author: Jacky Lui
+	* Date Last Modified: March 26, 2026
+	*/
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+	public static int min(int num1, int num2, int num3) {
+		int min = 0;
+	    if(num1 <= num2) {
+			if (num1 <= num3) {
+			min = num1;
+			}
+		}
+		if(num2 <= num1) {
+			if(num2 <= num3) {
+			min =num2;
+			}
+	    }
+		if(num3 <= num1) { 
+			if(num3 <= num2) {
+			min =num3;
+			}
+		}
+		return min;
 
+        }
+
+		public static boolean isLeapYear(int year) {
+			boolean isLeapYear = false;
+			if((year%4) == 0) {
+            if((year%100) == 0) {
+            if((year%400) == 0) {
+                isLeapYear=true;
+			}
+			else {
+				isLeapYear=false;
+			}
+			}
+			else {
+				isLeapYear=true;
+			}
+			}
+			else {
+				isLeapYear=false;
+			}
+			return isLeapYear;
+		}
+	
 }
